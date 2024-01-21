808093cc0a5af914503c6f4ddfed115087bc269a
Tawitg <108408219+Tawitg@users.noreply.github.com>
2024-01-21 16:46:26 +0000

    Added profile section and CV

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMobileMenu(){
+    const mobileMenu = document.querySelector(".hamburger-menu-links");
+    const mobileIcon = document.querySelector(".hamburger-icon");
+    mobileIcon.classList.toggle("open");
+    mobileMenu.classList.toggle("open");
+}
